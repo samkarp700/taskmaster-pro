17bb3504ac19353984ee16b03168a3da534208fa
Sam Karp <sfcypret@gmail.com>
2022-07-17 20:16:47 -0400

    ux enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var tasks = {};
 
+
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -160,7 +162,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -197,19 +199,25 @@ $(".card .list-group").sortable({
   tolerance: "pointer", 
   helper: "clone", 
   activate: function(event) {
-    console.log("activate", this);
+  $(this).addClass("dropover");
+  $(".bottom-trash").addClass("bottom-trash-drag");
+
   },
 
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-active");
+    
   },
 
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
+   
   },
 
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
+    
   }, 
   update: function(){
     // array to store the task data in
@@ -248,14 +256,16 @@ $("#trash").droppable({
   tolerance: "touch", 
   drop: function(event, ui) {
     ui.draggable.remove();
-    console.log("drop");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }, 
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
+    
+
   }, 
 
   out: function(event,ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -280,7 +290,15 @@ if (moment().isAfter(time)) {
 } else if (Math.abs(moment().diff(time,"days")) <= 2) {
   $(taskEl).addClass("list-group-item-warning");
 }
+console.log(taskEl);
 };
+
+setInterval(function (){
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+    
+  });
+}, 1800000);
 
  // load tasks for the first time
  loadTasks();
